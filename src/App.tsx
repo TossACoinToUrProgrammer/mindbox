60666463c0b0523a0b4d01c4432fd5e107568800
Arlen Manasov <arlen.manasov@icloud.com>
2023-09-07 21:35:38 +0600

    chore: add basic styles and scss variables

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoContainer from './components/todo-container';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='title'>todos</h1>
+      <TodoContainer />
+    </div>
+  );
+}
+
+export default App;
